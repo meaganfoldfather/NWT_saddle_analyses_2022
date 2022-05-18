5b895ad2deb4bee374a9b1946ebc4a6ce01093b2
MeaganOldfather <meagan.f.oldfather@gmail.com>
2022-05-18 15:20:34 -0600

    Incorperating colonization and extinctions

--- scripts/species_specific_responses.R
+++ scripts/species_specific_responses.R
@@ -101,11 +101,116 @@ veg_snow %>%
 # Geum is decreasing the less snow sites,  but it's thermal niche is ~0
 
 # Is the thermophilizastion patterns driven by species increases in abundance or decreases? Need to compare slope to thermal niches by rank --> use veg_snow_niche
-test <-
- left_join(species_slopes_snow, veg_snow_niche[,c(3,9:13)], by = "USDA_code") # This isn't working... creates huge df
-test
+# need to make a dataframe that is just the crosswalk from codes to names
+slope_niches <-
+ left_join(species_slopes_snow, distinct(veg_snow_niche[,c(3,9:13)]), by = "USDA_code")
+slope_niches
 
-# need to make a dataframe that is just the crosswalk from codes to names...
+slope_niches$SnowPersistence <- "Average Snow"
+slope_niches[slope_niches$snow_rank == 1, "SnowPersistence"] <- "Low Snow"
+slope_niches[slope_niches$snow_rank == 3, "SnowPersistence"] <- "High Snow"
+
+slope_niches %>% 
+  ggplot(aes(CLMtemp, slope, color = SnowPersistence))+
+  geom_point()+
+  theme_bw()+
+    theme_classic()+
+  ylab("Slope")+
+  xlab("Thermal Niche")+
+  facet_wrap(.~ SnowPersistence)+
+    scale_color_viridis_d(option = "turbo")+
+   theme(legend.title=element_blank(), text = element_text(size=16))
+# This plot is even more convincing that largely the patterns are driven by the two species discussed above, except maybe in low snow where some warm species are being lost too 
+
+slope_niches %>% 
+  filter(SnowPersistence == "Low Snow", slope < -0.1)  
+# These species are Sellegennal and Geum, which are decreasing
+
+# Investigate of losses/gain os species also driving community composition, or just changes in abudance 
+# calculate species-specific extinction and colonization rates
+rates <-
+veg_snow %>% 
+  group_by(SnowPersistence, plot, USDA_code) %>%  
+  summarise(
+  
+    #extinction
+    extinction = case_when(
+    sum(hits[year %in% 1990:1995]) > 0 & sum(hits[year %in% 2019:2020]) == 0 ~ 1,
+    sum(hits[year %in% 1990:1995]) > 0 & sum(hits[year == 2019:2020]) > 0 ~ 0),
+  
+    #colonization
+    colonization = case_when(
+    sum(hits[year %in% 1990:1995]) == 0 & sum(hits[year %in% 2019:2020]) > 0 ~ 1,
+    sum(hits[year %in% 1990:1995]) == 0 & sum(hits[year %in% 2019:2020]) == 0 ~ 0),
+    
+    #change in cover 
+    
+    cover_change = case_when( 
+      sum(hits) > 0 ~
+      (sum(hits[year %in% 2019:2020]) - sum(hits[year %in% 1990:1995]))),
+      # cover could also be done with hits ~ yr (impacted by mean average abundance?)
+      cover_slope = case_when( 
+      sum(hits) > 0 ~ lm(hits ~ year)$coefficients[2])
+    )
+
+rates_CNM <-
+  left_join(rates, distinct(veg_snow_niche[,c(3,9:13)]))
+rates_CNM
 
 
+# Models 
+# extinction temp
+fit_ext_temp <- glmer(extinction ~ CLMtemp*SnowPersistence + (1|USDA_code) + (1|plot), data = rates_CNM, family = "binomial")
+summary(fit_ext_temp)
+emtrends(fit_ext_temp, specs = "SnowPersistence", var = "CLMtemp")
 
+rates_CNM %>% 
+ggplot(aes(CLMtemp, extinction))+
+  geom_point()+
+  geom_smooth(method = "lm")+
+  theme_classic()+
+  facet_wrap(.~SnowPersistence)
+
+# colonization temp
+fit_col_temp <- glmer(colonization ~ CLMtemp*SnowPersistence + (1|USDA_code) + (1|plot), data = rates_CNM, family = "binomial")
+summary(fit_col_temp)
+emtrends(fit_col_temp, specs = "SnowPersistence", var = "CLMtemp")
+
+rates_CNM %>% 
+ggplot(aes(CLMtemp, colonization))+
+  geom_point()+
+  geom_smooth(method = "lm")+
+  theme_classic()+
+  facet_wrap(.~SnowPersistence)
+
+# Overall low snow sites have had more colonization and extinctions, but not directly related to the thermal niche of the species 
+
+
+# Does this calculation of cover change and slope tell a different story?
+# colonization temp
+fit_cov_temp <- lmer(cover_slope ~ CLMtemp*SnowPersistence + (1|USDA_code), data = rates_CNM)
+summary(fit_cov_temp)
+emtrends(fit_cov_temp, specs = "SnowPersistence", var = "CLMtemp")
+# No clear relationship, but still deschampsia and kobresia are the ones that change the most
+
+rates_CNM %>% 
+ggplot(aes(CLMtemp, cover_slope))+
+  geom_point()+
+  geom_smooth(method = "lm")+
+  theme_classic()+
+  facet_wrap(.~SnowPersistence)
+
+fit_cover_temp <- lmer(cover_change ~ CLMtemp*SnowPersistence + (1|USDA_code), data = rates_CNM)
+summary(fit_cover_temp)
+emtrends(fit_cover_temp, specs = "SnowPersistence", var = "CLMtemp")
+# There is a significant change negative relationship between thermal niche and chnage in cover in low snow areas!
+
+rates_CNM %>% 
+ggplot(aes(CLMtemp, cover_change))+
+  geom_point()+
+  geom_smooth(method = "lm")+
+  theme_classic()+
+  facet_wrap(.~SnowPersistence)
+
+
+     
